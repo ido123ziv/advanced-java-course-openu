6a38c55104146c2f36cf4a095bef4a7e7fb40035
idosh <idoshziv@gmail.com>
2023-03-11 17:29:02 +0200

    adding row in count to register

---
+++ mmn11/src/RowInAccount.java
@@ -0,0 +1,29 @@
+public class RowInAccount {
+    private double priceOfItem;
+    private int quantity;
+
+    private double totalSum;
+
+    public RowInAccount(double p, int q){
+        this.priceOfItem = p;
+        this.quantity = q;
+        this.totalSum = p * q;
+    }
+
+    public double getTotalSum() {
+        return totalSum;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "RowInAccount{" +
+                "priceOfItem=" + priceOfItem +
+                ", quantity=" + quantity +
+                ", totalSum=" + totalSum +
+                '}';
+    }
+}
