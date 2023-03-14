2948c7ffa2d9f2bc54b4c1451b20bbdeb43e874b
idosh <idoshziv@gmail.com>
2023-03-14 22:50:22 +0200

    drawing bars ready

---
+++ mmn11-fx/src/main/java/com/example/mmn11fx/DrawRectController.java
@@ -0,0 +1,74 @@
+package com.example.mmn11fx;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class DrawRectController {
+    @FXML
+    private Canvas canv;
+
+    private GraphicsContext gc;
+
+    public void initialize() {
+        gc = canv.getGraphicsContext2D();
+        List<Integer> li = data.getCurrentYear();
+        drawBars(li);
+    }
+
+    @FXML
+    void drawRect(ActionEvent event) {
+//        gc.setFill(Color.GRAY);
+        // x y width height
+//        gc.fillRect(0, 0, 100, 200);
+    }
+
+    public void drawBars(List<Integer> li){
+        int barCount = li.size(), delimiter = (barCount -1);
+        double xc = (canv.getWidth() - (delimiter * 10)) /barCount;
+        double yc = (canv.getHeight()) / 3;
+        int highestIndex = getHighestIndex(li), lowestIndex = getLowestIndex(li);
+        for (int i = 0; i < barCount ; i ++){
+           if (i == highestIndex) gc.setFill(Color.RED);
+           else if (i == lowestIndex) {
+               gc.setFill(Color.BLUE);
+           }
+           else gc.setFill(Color.GRAY); // if all are equal then make them gray
+           int yLocation = li.get(highestIndex) -  li.get(i);
+           gc.fillRect((i *  delimiter *2) + xc , yc + ( delimiter* yLocation), delimiter, (li.get(i) * delimiter));
+           gc.setFill(Color.BLACK);
+           gc.strokeText("" + (i + 1),(i *  delimiter * 2) + xc  ,delimiter + yc + ( delimiter* li.get(highestIndex)));
+           gc.strokeText("" + li.get(i) + "",(i *  delimiter *2) + xc  ,yc + ( delimiter* yLocation));
+
+        }
+//        gc.strokeLine(0, yc + li.get(highestIndex) * delimiter, delimiter, li.get(highestIndex) * delimiter );
+
+    }
+
+    public int getHighestIndex(List<Integer> li){
+        int max = Integer.MIN_VALUE;
+        for (Integer integer : li) {
+            if (integer > max) {
+                max = integer;
+
+            }
+        }
+        if (li.contains(max)) return li.indexOf(max);
+        return 0;
+    }
+    public int getLowestIndex(List<Integer> li){
+        int min = Integer.MAX_VALUE;
+        for (Integer integer : li) {
+            if (integer < min) {
+                min = integer;
+
+            }
+        }
+        if (li.contains(min)) return li.indexOf(min);
+        return 0;
+    }
+}
--- mmn11-fx/src/main/java/com/example/mmn11fx/data.java
+++ mmn11-fx/src/main/java/com/example/mmn11fx/data.java
@@ -1,12 +1,12 @@
 package com.example.mmn11fx;
 
+
 import java.util.*;
 
 public class data {
-    public static Map<String, List<Integer>> avgTemp = new HashMap<String,List<Integer>>() {{
+    public static Map<String, List<Integer>> avgTemp = new HashMap<>() {{
         put("2017", new ArrayList<>(Arrays.asList(
                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)));
-
         put("2018", new ArrayList<>(Arrays.asList(
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)));
         put("2019", new ArrayList<>(Arrays.asList(
@@ -16,5 +16,18 @@ public class data {
         put("2021", new ArrayList<>(Arrays.asList(
         4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)));
     }};
+    public static String currentYear;
 
+    public static List<Integer> getCurrentYear(){
+        Optional<String> keyOPtional = data.avgTemp.keySet().stream().findFirst();
+        if (keyOPtional.isPresent()) {
+            String key = keyOPtional.get();
+            data.currentYear = key;
+            return data.avgTemp.get(key);
+        }
+        // default
+        data.currentYear = "2021";
+        return data.avgTemp.get("2021");
+
+    }
 }
