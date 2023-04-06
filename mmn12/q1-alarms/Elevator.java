9155a96ba5c20bbd3edaeb4066a01c6627f4767a
idosh <idoshziv@gmail.com>
2023-04-06 17:44:52 +0300

    Adding Elevator.java

--- mmn12/q1-alarms/Elevator.java
+++ mmn12/q1-alarms/Elevator.java
@@ -1,17 +1,28 @@
 public class Elevator extends Alarm{
     private int floor;
 
+    /**
+     * Creates new elevator alarm
+     * @param address -> address of alarm
+     * @param floor -> floor of elevator alarm
+     */
     public Elevator(String address, int floor){
         super(address);
         this.floor = floor;
     }
 
+    /**
+     * sets the alarm
+     */
     @Override
     public void action() {
         super.action();
         System.out.println("Floor of alarm: " + floor);
     }
 
+    /**
+     * reset the floor for the elevator
+     */
     public void reset(){
         floor = 0;
     }
