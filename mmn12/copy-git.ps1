3edb95ea84d79ca7a66c5eca9f2a7d9f308dae54
idosh <idoshziv@gmail.com>
2023-04-06 22:33:00 +0300

    adapting to q2

--- mmn12/copy-git.ps1
+++ mmn12/copy-git.ps1
@@ -1,9 +1,14 @@
 # copy and git
 Param (
-  $Path
+  [Parameter(Mandatory)]$Path,
+  $Message
 )
-Copy-Item $Path -Destination "q1-alarms\"
+Copy-Item $Path -Destination "q2-bigint\"
 $FileName=$Path.split("\")[2]
-Write-Host "File created at path q1-alarms\$FileName"
-git add q1-alarms\$FileName
-git commit -m "Adding $FileName"
+Write-Host "File created at path q2-bigint\$FileName"
+git add q2-bigint\$FileName
+If (-Not $Message -eq '') {
+    git commit -m "Adding $Message function"
+} Else {
+    git commit -m "Adding $FileName"
+}
