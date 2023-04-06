1559c4db4ed46787847c9964e69675b1ede6fa31
idosh <idoshziv@gmail.com>
2023-04-06 17:47:11 +0300

    Adding Fire.java

--- mmn12/q1-alarms/Fire.java
+++ mmn12/q1-alarms/Fire.java
@@ -1,8 +1,14 @@
 public class Fire extends Smoke{
 
     private boolean isActive;
-    public Fire(String address, String op){
-        super(address, op);
+
+    /**
+     * creates an instance of fire alarm
+     * @param address -> address of alarm
+     * @param operator -> operator of alarm
+     */
+    public Fire(String address, String operator){
+        super(address, operator);
         isActive = true;
     }
 
