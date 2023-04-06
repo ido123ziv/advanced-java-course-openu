e0e70b91e5b36c4655982c2673e49b27cdda3c85
idosh <idoshziv@gmail.com>
2023-04-06 20:17:49 +0300

    Adding plus function

--- mmn12/q2-bigint/BigInt.java
+++ mmn12/q2-bigint/BigInt.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Map;
 
 public class BigInt implements Comparable{
     private ArrayList<Integer> bigIntList = new ArrayList<Integer>();
@@ -65,6 +66,95 @@ public class BigInt implements Comparable{
         this.isPositive=true;
     }
 
+    public BigInt plus(BigInt b){
+        if (this.isPositive == b.isPositive) // if they share a sign
+            return add(this,b);
+        if (this.isPositive)
+            return sub(this, b);
+        return sub(b, this);
+    }
+
+    /**
+     * adds two big ints
+     * @param b1 -> first big int
+     * @param b2 -> second big int
+     * @return -> the result of the sum of the two
+     */
+    private BigInt add(BigInt b1, BigInt b2){
+        ArrayList<Integer> newBigIntList = new ArrayList<Integer>();
+        int sum = 0, carry = 0;
+        int jointLength = Math.max(b1.bigIntList.size(), b2.bigIntList.size()); // go for the larger big int
+        for (int i = 0; i < jointLength; i ++){
+            int x1 = 0, x2 = 0;
+            if (b1.bigIntList.size() >= i) // if didn't finish b1
+                x1 = b1.bigIntList.get(i);
+            if (b2.bigIntList.size() >= i) // if didn't finish b2
+                x2 = b1.bigIntList.get(i);
+            sum = x1 + x2 + carry;
+            carry = sum / 10;  // this will set carry to 0 if sum is less than 10 otherwise will carry it
+            if (sum > 9){ // if sum is two or more digits we need to take just the units digit
+                sum = sum % 10;
+            }
+            newBigIntList.add(sum);
+        }
+        while (carry != 0){ // if carry remains
+            newBigIntList.add(carry % 10);
+            carry = carry / 10;
+        }
+        return new BigInt(newBigIntList, b1.isPositive);
+    }
+
+    /**
+     * subs a big int  form a big int
+     * @param b1 -> first big int
+     * @param b2 -> second big int
+     * @return -> the result of the subtraction
+     */
+    private BigInt sub(BigInt b1, BigInt b2){
+        int cmp = compareWithoutSign(b1, b2);
+        if (cmp == 0 && b1.isPositive == b2.isPositive)
+            return new BigInt(); // edge case, both BigInts are equal
+        BigInt bigger, smaller;
+        ArrayList<Integer> newBigIntList = new ArrayList<Integer>();
+        boolean newBigIntPositive;
+        int tmp = 0, sum;
+        if (cmp > 0){ // b1 > b2
+            bigger = b1;
+            smaller = b2;
+            newBigIntPositive= b1.isPositive;
+            // if b1 is positive and b2 is positive and smaller result will be positive
+            // if b1 is positive and b2 is negative result will be positive
+            // if b1 is negative and b2 is positive and smaller result will be negative
+            // if b1 is negative and b2 is negative, we know that b1 is bigger so the result will be negative
+        }
+        else { // b1 < b2 || |b2| == |b1|
+            bigger = b2;
+            smaller = b1;
+            // if b1 is positive and b2 is positive and bigger result will be negative
+            // if b1 is positive and b2 is negative result will be positive
+            // if b1 is negative and b2 is positive and smaller result will be negative
+            // if b1 is negative and b2 is negative, we know that b2 is bigger so the result will be positive
+            newBigIntPositive = !b2.isPositive;
+        }
+        int x1=0,x2=0;
+        for (int i = 1; i < b1.bigIntList.size() ;i++) {
+            x1 = bigger.bigIntList.get(i);
+            x2=0;
+            if (b2.bigIntList.size() >= i)
+                x2 = smaller.bigIntList.get(i);
+            sum = x1 - x2 + tmp;
+            tmp = 0;
+            while (sum + ((-tmp) * 10 ) < 0) {
+                tmp -= 1;
+            }
+            sum += ((-tmp) * 10);
+            newBigIntList.add(sum);
+        }
+        // removes 0's from beginning
+        while (newBigIntList.get(newBigIntList.size() - 1) == 0)
+            newBigIntList.remove(newBigIntList.size() -1 );
+        return new BigInt(newBigIntList,newBigIntPositive);
+    }
 
     /**
      *
@@ -89,15 +179,25 @@ public class BigInt implements Comparable{
      * @return -> if the values are equal
      */
     public int compareWithoutSign(BigInt b1){
+        return compareWithoutSign(this,b1);
+    }
+
+    /**
+     * determines which input BigInt is bigger by value to current bigInt
+     * @param b1 -> first big int
+     * @param b2 -> second big int
+     * @return -> who is bigger b1 is 1, b2 is -1, 0 is equal
+     */
+    public int compareWithoutSign(BigInt b1, BigInt b2){
  // numbers not same length are not equal
-        if (b1.bigIntList.size() > this.bigIntList.size())
+        if (b1.bigIntList.size() > b2.bigIntList.size())
             return -1;
-        if (b1.bigIntList.size() < this.bigIntList.size())
+        if (b1.bigIntList.size() < b2.bigIntList.size())
             return 1;
-        for (int i = this.bigIntList.size() -1; i >= 0; i --){
-            if (b1.bigIntList.get(i) > this.bigIntList.get(i))
+        for (int i = b2.bigIntList.size() -1; i >= 0; i --){
+            if (b1.bigIntList.get(i) > b2.bigIntList.get(i))
                 return -1;
-            if (b1.bigIntList.get(i) < this.bigIntList.get(i))
+            if (b1.bigIntList.get(i) < b2.bigIntList.get(i))
                 return 1;
         }
         return 0;
