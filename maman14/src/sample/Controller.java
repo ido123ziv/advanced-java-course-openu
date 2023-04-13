81b5149b35c73836ef0549653cc010bad9e82431
EliGandin <87780727+EliGandin@users.noreply.github.com>
2023-04-13 10:30:38 +0300

    Finished project, includes date validation and saving/loading from file

--- maman14/src/sample/Controller.java
+++ maman14/src/sample/Controller.java
@@ -2,9 +2,12 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
+import java.io.*;
 import java.util.HashMap;
 
 public class Controller {
@@ -24,18 +27,22 @@ public class Controller {
     private HashMap<Date, String> map;
 
     @FXML
-    void setReminder(ActionEvent event) {
-        if (tf.getText().isBlank()) {
-            tf.setText((map.get(new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue()))));
-        } else {
-            map.put((new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue())),tf.getText());
-        }
+    void saveReminder(ActionEvent event) {
+        Date d = new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue());
+        if (isValid(d))
+            map.put((new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue())), tf.getText());
+        tf.setText("");
+    }
+
+    @FXML
+    void saveToFile(ActionEvent event) {
+        saveToFile();
     }
 
     @FXML
     void show(ActionEvent event) {
-        if (cbDay.getValue() != null && cbMonth.getValue() != null && cbYear.getValue() != null) {
-            Date d = new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue());
+        Date d = new Date(cbDay.getValue(), cbMonth.getValue(), cbYear.getValue());
+        if (isValid(d)) {
             if (map.containsKey(d)) {
                 tf.setText((map.get(d)));
             } else {
@@ -44,22 +51,109 @@ public class Controller {
         }
     }
 
+
+    @FXML
+    void load(ActionEvent event) {
+        loadFile();
+    }
+
     public void initialize() {
-        map = new HashMap<Date,String>();
+        map = new HashMap<Date, String>();
         setComboBoxes();
     }
 
     public void setComboBoxes() {
-        for (int i = 0; i < 51; i++) {
+        final int MAX_DAY = 31, MAX_MONTH = 12, START_YEAR = 2023, MAX_YEAR = 2050;
+        for (int i = START_YEAR; i <= MAX_YEAR; i++) {
             cbYear.getItems().add(1999 + i);
         }
+        cbYear.setValue(2023);
 
-        for (int i = 1; i < 13; i++) {
+        for (int i = 1; i <= MAX_MONTH; i++) {
             cbMonth.getItems().add(i);
         }
+        cbMonth.setValue(1);
 
-        for (int i = 1; i < 32; i++) {
+        for (int i = 1; i <= MAX_DAY; i++) {
             cbDay.getItems().add(i);
+        }
+        cbDay.setValue(1);
+    }
+
+    public boolean isValid(Date date) {
+        int month = date.getMonth();
+        if (month == 2) {
+            if (date.getYear() % 100 != 0) {
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("Error");
+                a.setHeaderText(null);
+                a.setContentText("February " + date.getYear() + " Has Only 28 Days");
+                a.showAndWait();
+                return false;
+            }
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11) {
+            if (date.getDay() == 31) {
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("Error");
+                a.setHeaderText(null);
+                a.setContentText("This Month Has Only 30 Days");
+                a.showAndWait();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private File getFile() {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Please Choose File");
+        fc.setInitialDirectory(new File("."));
+        return fc.showOpenDialog(null);
+    }
+
+    private void loadFile() {
+        File f = getFile();
+        FileInputStream fi = null;
+        ObjectInputStream in = null;
+        if (f != null) {
+            try {
+                fi = new FileInputStream("Reminders.txt");
+                in = new ObjectInputStream(fi);
+                map = (HashMap<Date, String>) in.readObject();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    fi.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void saveToFile() {
+        File f = getFile();
+        FileOutputStream fo = null;
+        ObjectOutputStream out = null;
+        if (f != null) {
+            try {
+                fo = new FileOutputStream("Reminders.txt");
+                out = new ObjectOutputStream(fo);
+                out.writeObject(map);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    out.close();
+                    fo.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- maman14/src/sample/Date.java
+++ maman14/src/sample/Date.java
@@ -1,8 +1,11 @@
 package sample;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public class Date {
+public class Date implements Serializable {
     private int day;
     private int month;
     private int year;
@@ -49,4 +52,75 @@ public class Date {
     public int hashCode() {
         return Objects.hash(day, month, year);
     }
+
+    public static List<Date> getValidDates(int startYear, int endYear) {
+        List<Date> validDates = new ArrayList<>();
+
+        for (int year = startYear; year <= endYear; year++) {
+            for (int month = 1; month <= 12; month++) {
+                int daysInMonth;
+                switch (month) {
+                    case 2:
+                        daysInMonth = isLeapYear(year) ? 29 : 28;
+                        break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        daysInMonth = 30;
+                        break;
+                    default:
+                        daysInMonth = 31;
+                        break;
+                }
+                for (int day = 1; day <= daysInMonth; day++) {
+                    Date date = new Date(year, month, day);
+                    if (date.isValidDate()) {
+                        validDates.add(date);
+                    }
+                }
+            }
+        }
+
+        return validDates;
+    }
+
+    public boolean isValidDate() {
+        // Check if year is valid
+        if (year < 1) {
+            return false;
+        }
+
+        // Check if month is valid
+        if (month < 1 || month > 12) {
+            return false;
+        }
+
+        // Check if day is valid
+        int daysInMonth;
+        switch (month) {
+            case 2:
+                daysInMonth = isLeapYear(year) ? 29 : 28;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = 30;
+                break;
+            default:
+                daysInMonth = 31;
+                break;
+        }
+        return day >= 1 && day <= daysInMonth;
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else return year % 4 == 0;
+    }
 }
+
