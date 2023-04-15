1268b7c0a57e98f4210647f967919023f8b90957
idosh <idoshziv@gmail.com>
2023-04-15 13:59:22 +0300

    handle files

---
+++ mmn13/q2/FilesHandler.java
@@ -0,0 +1,58 @@
+package com.example.mmn13;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FilesHandler {
+
+    public static ArrayList<Item> getMenu(){
+        ArrayList<Item> menuList = new ArrayList<>();
+        try {
+            File file = new File("src\\main\\java\\com\\example\\mmn13\\menu.txt");
+            Scanner scanner = new Scanner(file);
+            String [] strings = new String[3];
+            int counter = 0;
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.compareTo("---") != 0){
+                    strings[counter] = line;
+                    counter ++;
+                }
+                else {
+                    counter = 0;
+                    menuList.add(new Item(strings[0],strings[1], Double.parseDouble(strings[2]) ));
+                    strings = new String[3];
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return menuList;
+    }
+//    private static File getFile() {
+//        FileChooser fc = new FileChooser();
+//        fc.setTitle("Please Choose File");
+//        fc.setInitialDirectory(new File("."));
+//        return fc.showOpenDialog(null);
+//    }
+
+    public static void saveToFile(String name, String Content){
+        try {
+            File myObj = new File(name);
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+                FileWriter myWriter = new FileWriter(name);
+                myWriter.write(Content);
+                myWriter.close();
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
