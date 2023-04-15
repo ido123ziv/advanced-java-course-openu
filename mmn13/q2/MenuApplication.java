41904a607bdd79a75a19c111efcca28716ecc7d3
idosh <idoshziv@gmail.com>
2023-04-15 15:51:09 +0300

    adding fxml

--- mmn13/q2/MenuApplication.java
+++ mmn13/q2/MenuApplication.java
@@ -11,7 +11,7 @@ public class MenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- mmn13/q2/MenuController.java
+++ mmn13/q2/MenuController.java
@@ -19,10 +19,10 @@ public class MenuController {
     private TextField [] types;
 
     @FXML
-    private ComboBox<Integer> cbItem;
+    private ComboBox<Integer> [] cbItems;
 
     @FXML
-    private CheckBox control;
+    private CheckBox [] checkBoxes;
     @FXML
     protected void orderPressed() {
         System.out.println("Hello");
@@ -33,15 +33,36 @@ public class MenuController {
         HashMap<mealCourse, ArrayList<Item>> menuItems = menu.getMenu();
         ArrayList<mealCourse> keys = new ArrayList<>(menuItems.keySet());
         types = new TextField[keys.size()];
-        double textSize = grid.getPrefWidth();
+        checkBoxes = new CheckBox [keys.size()];
+        cbItems = new ComboBox[keys.size()];
+        double textSize = grid.getPrefWidth() / (keys.size() * keys.size());
         double textHeight = grid.getPrefHeight() / (keys.size() * keys.size());
-
+        int lastIndex = 0, colCounter = 0;
         for (int i =0; i < keys.size(); i ++){
             types[i] = new TextField(keys.get(i).name());
             types[i].setPrefHeight(textHeight);
             types[i].setPrefWidth(textSize);
             types[i].setId(keys.get(i).name());
-//            grid.add(types[i], );
+            grid.add(types[i], colCounter, lastIndex);
+            for (Item item : menuItems.get(keys.get(i))){
+//                colCounter++;
+                lastIndex++;
+                TextField tf = new TextField(item.getDescription());
+                tf.setPrefHeight(textHeight);
+                tf.setPrefWidth(textSize);
+                grid.add(tf, colCounter,lastIndex);
+                CheckBox cb = new CheckBox();
+                colCounter++;
+                grid.add(cb, colCounter ,lastIndex);
+                ComboBox<Integer> comboBox = new ComboBox<>();
+                colCounter++;
+                grid.add(comboBox, colCounter ,lastIndex);
+                lastIndex++;
+            }
+            colCounter = 0;
         }
+
+
+
     }
 }
