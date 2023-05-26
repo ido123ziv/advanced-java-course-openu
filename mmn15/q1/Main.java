3ffd4af9859f78c692cc899adfa9b88134a37283
idosh <idoshziv@gmail.com>
2023-05-27 00:10:37 +0300

    documenting

--- mmn15/q1/Main.java
+++ mmn15/q1/Main.java
@@ -1,53 +1,63 @@
 package q1;
-import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * Main Class
+ * Ido Ziv
+ * 26/05/2023
+ */
 public class Main {
 
+    /**
+     * runs the program
+     * @param args to run main
+     */
     public static void main(String [] args){
+        // init the arrays
         int [][] mat1 = Utils.matrixGenerator(5,3);
         int [][] mat2 = Utils.matrixGenerator(3,4);
         int [][] result = Utils.resultMatrixGenerator(mat1.length,mat2[0].length);
 
-        System.out.println("mat1.length: " + mat1.length + " mat1[0].length: " + mat1[0].length);
-
-
+        //printing arrays for better UI
         System.out.println("Printing Arrays: \n------------------");
         System.out.println("mat1: ");
         Utils.printMat(mat1);
         System.out.println("----------------\nmat2: ");
         Utils.printMat(mat2);
         System.out.println("----------------\n");
-        Date start = new Date();
 
+        Date start = new Date();
+        // executing multiplication
         multiply(mat1,mat2, result);
 
         Date end = new Date();
         System.out.println("Process took: " + (end.getTime() - start.getTime()) + " milliseconds" );
+
         System.out.println("Result: ");
         Utils.printMat(result);
     }
 
+    /**
+     * this method handles the multiplication using threads
+     * @param matrix1 -> matrix a (size n x m)
+     * @param matrix2 -> matrix b (size m x p)
+     * @param result -> matrix of the result (size n x p)
+     */
     public static void multiply(int [][] matrix1, int [][] matrix2,int [][] result){
         if (matrix1[0].length != matrix2.length)
-            System.out.println("Can't Multiply!");
+            System.out.println("Can't Multiply!"); // if the columns of matrix 2 doesn't match the rows of matrix 1
         else {
-//            ArrayList<Thread> threadsList = new ArrayList<>();
-            ThreadsCreator tc = new ThreadsCreator(result);
+            Monitor monitor = new Monitor(result);
             int matrix1Rows = matrix1.length;
             int matrix2Cols = matrix2[0].length;
             RowMultiplyJob[][] rowMultiplyJobs = new RowMultiplyJob[matrix1Rows][matrix2Cols];
-            System.out.println("rows: " + matrix1Rows + " cols: " + matrix2Cols + " mult " + (matrix1Rows * matrix2Cols));
-            for (int i =0; i < matrix1Rows; i ++) {
+            for (int i =0; i < matrix1Rows; i ++) { // initializing threads
                 for (int j = 0; j < matrix2Cols; j ++){
-                    rowMultiplyJobs[i][j] = new RowMultiplyJob(result, matrix1, matrix2, i , j, tc);
+                    rowMultiplyJobs[i][j] = new RowMultiplyJob(result, matrix1, matrix2, i , j, monitor);
                     rowMultiplyJobs[i][j].start();
-//                    Thread thread = new Thread(task);
-//                    thread.start();
-//                    threadsList.add(thread);
                 }
             }
-            tc.waitForAll();
+            monitor.waitForAll(); // waiting for all threads to finish before printing
         }
     }
 
--- mmn15/q1/RowMultiplyJob.java
+++ mmn15/q1/RowMultiplyJob.java
@@ -1,18 +1,30 @@
 package q1;
 
+/**
+ * Simple implementation of Rows Multiplication
+ */
 public class RowMultiplyJob extends Thread {
 
-    private final int[][] result;
-    private final int[][] matrix1;
-    private final int[][] matrix2;
-    private final int mat1Row;
-    private final int mat2Col;
+    private final int[][] result; // final matrix to store the result
+    private final int[][] matrix1; // first matrix to multiply
+    private final int[][] matrix2; // second matrix to multiply
+    private final int mat1Row; // current row in work
+    private final int mat2Col; // current col in work
 
-    private ThreadsCreator monitor;
+    private final Monitor monitor; // monitor to use in these threads
 
-    private final int id;
+    private final int id; // thread id
 
-    public RowMultiplyJob(int[][] resultMatrix,int[][] inMatrix1,int[][] inMatrix2, int currentRow, int currentCol, ThreadsCreator m){
+    /**
+     * Constructor that builds an instance of a row multiplication thread that calculates a column multiply by a row
+     * @param resultMatrix -> where to store the result
+     * @param inMatrix1 -> first matrix to multiply
+     * @param inMatrix2 -> second matrix to multiply
+     * @param currentRow -> current row in work
+     * @param currentCol -> current col in work
+     * @param m -> monitor to use in these threads
+     */
+    public RowMultiplyJob(int[][] resultMatrix,int[][] inMatrix1,int[][] inMatrix2, int currentRow, int currentCol, Monitor m){
         result = resultMatrix;
         matrix1 = inMatrix1;
         matrix2 = inMatrix2;
@@ -22,18 +34,18 @@ public class RowMultiplyJob extends Thread {
         monitor = m;
     }
 
+    /**
+     * Implementation of run of the thread
+     * Calculates the cell in the result by multiplying a row by a column
+     * waiting for it's turn to print the result
+     */
     @Override
     public void run() {
         super.run();
-//        System.out.println("Thread Id: " + id + " Started!");
         result[mat1Row][mat2Col] = 0;
         for (int j =0; j < matrix1[0].length; j++){
             result[mat1Row][mat2Col] += matrix1[mat1Row][j] * matrix2[j][mat2Col];
         }
-//        System.out.println(id + " Entering wait");
-//        if (mat2Col == matrix2[0].length -1)
-//            monitor.donePrint();
-
         monitor.waitForPrint(id);
         System.out.println("Thread Id: " + id + " got the result: " + result[mat1Row][mat2Col]);
         monitor.donePrint();
