4c68b6d4995cf69db326475113369c02eee75397
idosh <idoshziv@gmail.com>
2023-05-30 08:27:54 +0300

    adding basic logic

--- mmn15/q2/src/Java/q2/TrafficLightController.java
+++ mmn15/q2/src/Java/q2/TrafficLightController.java
@@ -1,29 +1,73 @@
 package q2;
-import java.io.FileNotFoundException;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-public class TrafficLightController {
+import javafx.scene.shape.Rectangle;
+
+public class TrafficLightController{
 
 
+    public Circle LeftTopCirc, RightTopCirc, TopBottomCirc, TopTopCirc;
+    public Circle LeftBottomCirc,  RightBottomCirc, BottomTopCirc, BottomBottomCirc;
+    public Rectangle LeftTopRec, RightTopRec, TopTopRec, BottomTopRec;
+    public Rectangle  LeftBottomRec , RightBottomRec, TopBottomRec, BottomBottomRec;
     @FXML
-    private Circle light;
+    private Circle[] topCirc; // top
+    private Circle[] bottomCirc;
+    private Circle[] northSouthTop;
 
-    public void setFillGreen() {
-        light.setFill(Color.LIMEGREEN);
+    private Rectangle[] topRec;
+    private Rectangle[] bottomRec;
+    private int side;
+    private boolean stop;
+
+    public void initialize() throws InterruptedException {
+        topCirc = new Circle[] {LeftBottomCirc, RightTopCirc, TopTopCirc, BottomBottomCirc};
+        bottomCirc = new Circle[] {BottomTopCirc, BottomBottomCirc, LeftBottomCirc, RightBottomCirc};
+        topRec = new Rectangle[] {LeftTopRec, RightTopRec, TopTopRec, BottomTopRec};
+        bottomRec = new Rectangle[]{LeftBottomRec, RightBottomRec, TopBottomRec, BottomBottomRec};
+        side = 1;
+        stop = false;
+        System.out.println("Initializing!");
+        TrafficLightThread ta = new TrafficLightThread(this);
+        ta.start();
+//        flip();
     }
 
-    public void setFillYellow() {
-        light.setFill(Color.YELLOW);
+    public boolean isStop() {
+        return stop;
     }
 
-    public void setFillRed() {
-        light.setFill(Color.RED);
+    public void flip(){
+        System.out.println("Flipping!");
+        if (side > 0){
+          TopTopCirc.setFill(Color.WHITE);
+          BottomTopCirc.setFill(Color.WHITE);
+          TopBottomCirc.setFill(Color.LIME);
+          BottomBottomCirc.setFill(Color.LIME);
+
+          LeftTopCirc.setFill(Color.RED);
+          RightTopCirc.setFill(Color.RED);
+          RightBottomCirc.setFill(Color.WHITE);
+          LeftBottomCirc.setFill(Color.WHITE);
+        }
+        else{
+          TopTopCirc.setFill(Color.LIME);
+          BottomTopCirc.setFill(Color.LIME);
+          TopBottomCirc.setFill(Color.WHITE);
+          BottomBottomCirc.setFill(Color.WHITE);
+
+          LeftTopCirc.setFill(Color.WHITE);
+          RightTopCirc.setFill(Color.WHITE);
+          RightBottomCirc.setFill(Color.RED);
+          LeftBottomCirc.setFill(Color.RED);
+        }
+        side = side * -1;
     }
 
-    public void setFillWhite() {
-        light.setFill(Color.WHITE);
+    public void toStop(){
+        stop = true;
     }
+
 }
--- mmn15/q2/src/Java/q2/TrafficLightThread.java
+++ mmn15/q2/src/Java/q2/TrafficLightThread.java
@@ -6,24 +6,16 @@ public class TrafficLightThread extends Thread{
         trafficGUI = gui;
     }
     public void run() {
-        trafficGUI.setFillGreen();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        System.out.println("Thread Running!");
+        super.run();
+        while (!trafficGUI.isStop()){
+            System.out.println("Whiling!");
+            trafficGUI.flip();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        trafficGUI.setFillYellow();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        trafficGUI.setFillRed();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        trafficGUI.setFillWhite();
     }
 }
