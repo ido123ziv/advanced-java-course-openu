9679cdd3fbf47c1f3b74fb955b322517df3d1881
idosh <idoshziv@gmail.com>
2023-05-30 08:27:10 +0300

    adding UI components

--- mmn15/q2/src/Java/q2/TrafficLightApplication.java
+++ mmn15/q2/src/Java/q2/TrafficLightApplication.java
@@ -9,7 +9,7 @@ public class TrafficLightApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TrafficLightApplication.class.getResource("trafficLight.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
